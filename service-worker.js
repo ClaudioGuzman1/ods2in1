fd8a74a6fab17bd49de52b5a1597ec690b039274
ClaudioGuzman1 <89861372+ClaudioGuzman1@users.noreply.github.com>
2026-01-21 15:11:38 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE = "offline-v1";
+
+const OFFLINE_ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-256.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(OFFLINE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
